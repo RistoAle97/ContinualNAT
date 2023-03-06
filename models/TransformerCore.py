db532add1be4b8b5943ee9561f39e2aac32f04af
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2023-03-06 20:13:21 +0100

    Fixed a small error in positional encoding

--- models/TransformerCore.py
+++ models/TransformerCore.py
@@ -15,22 +15,11 @@ class PositionalEncoding(nn.Module):
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
-        self.register_buffer("positional_encoding", pe)
+        self.register_buffer("pe", pe)
 
     def forward(self, x) -> torch.Tensor:
         x = x + self.pe[:, x.size(1)]
         return self.dropout(x)
-
-
-'''def positional_encoding(x: torch.Tensor, d_model: int = 512, max_len: int = 5000) -> torch.Tensor:
-    position = torch.arange(max_len).unsqueeze(1)
-    div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-    pe = torch.zeros(max_len, d_model)
-    pe[:, 0::2] = torch.sin(position * div_term)
-    pe[:, 1::2] = torch.cos(position * div_term)
-    pe = pe.unsqueeze(0)
-    x = x + pe[:, x.size(1)]  # (batch_size, seq_len, d_model)
-    return x'''
 
 
 class TransformerCore(nn.Module):
