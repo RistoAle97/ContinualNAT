Some fixes for the CMLM masking

src/models/CMLM.py
@@ -20,13 +20,10 @@ class CMLM(TransformerCore):
                  layer_norm_eps: float = 1e-6,
                  norm_first: bool = False,
                  share_embeddings_src_tgt: bool = True,
-                 share_embeddings_tgt_out: bool = True,
-                 mask_token_id: int = 250026) -> None:
+                 share_embeddings_tgt_out: bool = True) -> None:
         super().__init__(src_vocab_size, tgt_vocab_size, d_model, n_heads, num_encoder_layers, num_decoder_layers,
                          dim_ff, dropout, layer_norm_eps, norm_first, share_embeddings_src_tgt,
                          share_embeddings_tgt_out)
-        # Parameters
-        self.mask_token_id = mask_token_id
 
         # Pooler layer after the encoder to predict the target sentence length
         self.pooler = Pooler(d_model)
@@ -61,16 +58,44 @@ class CMLM(TransformerCore):
 
         # Encoder and decoder
         e_output = self.encoder(e_input, None, e_pad_mask)
-        predicted_length = self.pooler(e_output, True).unsqueeze(1)  # (batch_size, 1)
+        predicted_length = self.pooler(e_output)  # (batch_size, seq_len)
         d_output = self.decoder(d_input, e_output, None, None, d_pad_mask, e_pad_mask)
 
         # Linear output
         output = self.linear_output(d_output)  # (batch_size, seq_len, tgt_vocab_size)
         return output, predicted_length
 
-    def _mask_predict(self, x):
-        pass
+    def predict_target_length(self, e_output: torch.Tensor) -> torch.Tensor:
+        predicted_length = self.pooler(e_output, True).unsqueeze(1)
+        return predicted_length
 
-    def generate(self, x: torch.Tensor, sos_token_id: int, iterations: int = 10) -> torch.Tensor:
-        self._mask_predict(x)
-        pass
+    def _mask_predict(self,
+                      input_ids: torch.Tensor,
+                      sos_token_id: int,
+                      pad_token_id: int,
+                      mask_token_id: int,
+                      max_iteration: int = 10) -> torch.Tensor:
+        with torch.no_grad():
+            # Parameters
+            batch_size, seq_len = input_ids.size()
+            device = next(self.parameters()).device
+
+            # Encode the input tokens
+            e_pad_mask = (input_ids == pad_token_id).to(device)
+            encodings = self.encode(input_ids, e_pad_mask=e_pad_mask)
+
+            # Predict the target lengths and take the largest one
+            target_length = self.predict_target_length(encodings)
+            max_predicted_length = target_length.max()
+
+            output = torch.ones(batch_size, max_predicted_length.item(), dtype=torch.int,
+                                device=device).fill_(pad_token_id)
+            output[:, 0] = sos_token_id
+
+    def generate(self,
+                 input_ids: torch.Tensor,
+                 sos_token_id: int,
+                 pad_token_id: int,
+                 mask_token_id: int,
+                 iterations: int = 10) -> torch.Tensor:
+        return self._mask_predict(input_ids, sos_token_id, pad_token_id, mask_token_id, iterations)

src/modules/pooling.py
@@ -31,9 +31,9 @@ class Pooler(nn.Module):
 
     def forward(self, e_output: torch.Tensor, length_prediction: bool = False) -> torch.Tensor:
         out = self.linear(e_output[:, 0, :])
-        out = F.tanh(out)
+        out = nn.functional.tanh(out)
         if length_prediction:
             out = F.log_softmax(out, -1)
-            out.argmax(-1)
+            out = out.argmax(-1)
 
         return out