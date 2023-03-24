be04745d02c4f78d04f7276bd9a1158afa45f5c7
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2023-03-25 00:47:49 +0100

    Improved CMLM masking for the collator

--- src/data/collators.py
+++ src/data/collators.py
@@ -4,7 +4,7 @@ import numpy as np
 from transformers import PreTrainedTokenizer
 from transformers.utils import PaddingStrategy, TensorType
 from src.utils import shift_tokens_right
-from typing import Dict, List, Union
+from typing import Dict, Union
 
 
 class BatchCollator:
@@ -17,7 +17,7 @@ class BatchCollator:
                  add_special_tokens: bool = True,
                  return_tensors: Union[str, TensorType, None] = "pt",
                  use_language_tokens: bool = True,
-                 shift_labels_right: bool = True,
+                 is_mlm: bool = False,
                  use_cls_token: bool = False) -> None:
         """
         Standard collator, its work consists in tokenizing the source and target sentences, batching them and creating
@@ -29,12 +29,14 @@ class BatchCollator:
         :param add_special_tokens: whether to use special tokens during the tokenization (default=True)-
         :param return_tensors: type of tensors from the tokenizer (default="pt").
         :param use_language_tokens: whether to use language tokens by the tokenizer (default=True).
-        :param shift_labels_right: if the labels must be shifted in order to create the decoder inputs (default=True).
+        :param is_mlm: whether the collator is used for a masked language model (MLM), it False then we're dealing with
+            a causal model and the labels must be shifted to the right in order to create the decoder inputs
+            (default=False).
         :param use_cls_token: whether to add the cls token at the beginnning of the source sentences (default=False).
         """
         self.tokenizer = tokenizer
         self.use_language_tokens = use_language_tokens
-        self.shift_labels_right = shift_labels_right
+        self.is_mlm = is_mlm
         self.use_cls_token = use_cls_token
         self.collator_state = {"truncation": truncation, "max_length": max_length, "padding": padding,
                                "add_special_tokens": add_special_tokens, "return_tensors": return_tensors}
@@ -61,15 +63,23 @@ class BatchCollator:
             labels_batch = torch.where(labels_batch == tgt_lang_token, pad_token, labels_batch)
 
         # Create decoder input ids
-        if self.shift_labels_right:
-            if self.use_language_tokens and tokenizer_has_lang_codes:
-                pad_token_id = self.tokenizer.pad_token_id
-                decoder_start_token_id = self.tokenizer.lang_code_to_id[self.tokenizer.tgt_lang]
-                decoder_input_ids_batch = shift_tokens_right(labels_batch, pad_token_id, decoder_start_token_id)
+        if not self.is_mlm:
+            if self.use_language_tokens and tokenizer_has_lang_codes and labels_batch[0, 0]:
+                tgt_lang_token_id = self.tokenizer.lang_code_to_id[self.tokenizer.tgt_lang]
+                starts_with_tgt_lang_token = labels_batch[0, 0].eq(tgt_lang_token_id)
+                if starts_with_tgt_lang_token:
+                    # If the labels start with the tgt lang token we perform the usual shift
+                    decoder_input_ids_batch = labels_batch[:, :-1]
+                    labels_batch = labels_batch[:, 1:]
+                else:
+                    # Otherwise we move the tgt lang token to the first position in a MBart fashion
+                    decoder_input_ids_batch = shift_tokens_right(labels_batch, self.tokenizer.pad_token_id)
             else:
+                # The usual shift seen for causal language models
                 decoder_input_ids_batch = labels_batch[:, :-1]
                 labels_batch = labels_batch[:, 1:]
         else:
+            # This is applied for masked language models such as CMLM
             decoder_input_ids_batch = labels_batch.detach().clone()
 
         return {"input_ids": input_ids_batch, "labels": labels_batch, "decoder_input_ids": decoder_input_ids_batch}
@@ -100,60 +110,59 @@ class BatchCollatorCMLM(BatchCollator):
         :param train: whether the collator is used during training (default=False).
         """
         super().__init__(tokenizer, truncation, max_length, padding, add_special_tokens, return_tensors,
-                         use_language_tokens, False, True)
+                         use_language_tokens, True, True)
         # Parameters
         self.train = train
 
-    def __mask_target(self, tgt: torch.Tensor) -> Dict[str, Union[torch.Tensor, List[int]]]:
-        # Retrieve all the special tokens from the tokenizer and its mask id
-        tokenizer_special_tokens = self.tokenizer.all_special_ids
-        tokenizer_special_tokens.remove(self.tokenizer.bos_token_id)  # sos can be masked
-        tokenizer_special_tokens.remove(self.tokenizer.eos_token_id)  # eos can be masked
-        mask_id = self.tokenizer.mask_token_id
+    def __mask_target(self, labels: torch.Tensor, decoder_input_ids: torch.Tensor) -> Dict[str, torch.Tensor]:
+        batch_size, seq_len = labels.size()
 
-        # Initialize decoder inputs and labels
-        decoder_input_ids = tgt.new(tgt.tolist())
-        labels = tgt.new(tgt.size()).fill_(self.tokenizer.pad_token_id)
+        # Retrieve all the special tokens from the tokenizer and its mask id
+        if self.tokenizer.mask_token_id is None:
+            raise ValueError("For the CMLM model you should use a tokenizer that has a mask token defined.")
+
+        mask_token_id = self.tokenizer.mask_token_id
+
+        # Build the special tokens mask and get how many of them are there for each sentence
+        special_tokens_masks = [self.tokenizer.get_special_tokens_mask(sentence, already_has_special_tokens=True)
+                                for sentence in labels]
+        special_tokens_masks = torch.tensor(special_tokens_masks)  # 1 if special token, 0 otherwise
+        n_special_tokens = torch.sum(special_tokens_masks, dim=-1)
 
         # At least one token per each sentence should be masked
         min_masks = 1
 
-        # Compute the length of the target sentences without taking special tokens into account
-        n_special_tokens = torch.sum(torch.isin(tgt, torch.tensor(tokenizer_special_tokens)), dim=-1)
-        tgt_lengths = tgt.shape[-1] - n_special_tokens
+        # Keep the indexes of those tokens that can be masked and the number of such tokens
+        maskable_tokens_idxs = [(mask == 0).nonzero(as_tuple=True)[0].tolist() for mask in special_tokens_masks]
+        n_maskable_tokens = seq_len - n_special_tokens
 
         # Mask tokens loop
-        n_masks = []
-        mask_idxs = []
         if self.train:
             # At training time we mask out a number of tokens in [1, seq_len - n_special_tokens] from the decoder inputs
-            for i, tgt_length in enumerate(tgt_lengths):
-                if min_masks < tgt_length:
-                    sample_size = np.random.randint(min_masks, tgt_length)
-                else:
-                    sample_size = min_masks
+            labels = labels.new(batch_size, seq_len).fill_(self.tokenizer.pad_token_id)
+            for i, max_tokens_to_mask in enumerate(n_maskable_tokens):
+                if max_tokens_to_mask > 0:
+                    # Sample the number of tokens to mask with a uniform distribution
+                    sample_size = np.random.randint(min_masks, max_tokens_to_mask + 1)
 
-                n_masks.append(sample_size)
-                masks = random.sample(range(tgt_length), sample_size)
-                decoder_input_ids[i, masks] = mask_id
-                labels[i, masks] = tgt[i, masks]
-                mask_idxs.append(masks)
+                    # Sample the idxs to mask
+                    masks = random.sample(maskable_tokens_idxs[i], sample_size)
+
+                    # Mask the decoder inputs
+                    labels[i, masks] = decoder_input_ids[i, masks]
+                    decoder_input_ids[i, masks] = mask_token_id
+                else:
+                    labels[i] = decoder_input_ids[i]
         else:
             # At inference time we mask the entire decoder inputs
-            for i, tgt_length in enumerate(tgt_lengths):
-                decoder_input_ids[i, :tgt_length] = mask_id
-                mask_idxs.append(list(np.arange(0, tgt_length)))
+            for i, maskable_tokens in enumerate(maskable_tokens_idxs):
+                decoder_input_ids[i, maskable_tokens] = mask_token_id
 
-            labels = tgt
-            n_masks = tgt_lengths.tolist()
-
-        return {"decoder_input_ids": decoder_input_ids, "labels": labels, "mask_idxs": mask_idxs,
-                "n_masks": n_masks}
+        return {"labels": labels, "decoder_input_ids": decoder_input_ids, "lengths": n_maskable_tokens}
 
     def __call__(self, batch):
         tokenized_batch = super().__call__(batch)
-        input_ids, labels = tokenized_batch["input_ids"], tokenized_batch["labels"]
-        masked_target = self.__mask_target(labels)
-        return {"input_ids": input_ids, "decoder_input_ids": masked_target["decoder_input_ids"],
-                "labels": masked_target["labels"], "mask_idxs": masked_target["mask_idxs"],
-                "n_masks": masked_target["n_masks"]}
+        input_ids = tokenized_batch["input_ids"]
+        masked_target = self.__mask_target(tokenized_batch["labels"], tokenized_batch["decoder_input_ids"])
+        return {"input_ids": input_ids, "labels": masked_target["labels"],
+                "decoder_input_ids": masked_target["decoder_input_ids"], "lengths": masked_target["lengths"]}
