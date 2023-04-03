Fixed small error for causal masking

src/inference/strategies.py
@@ -12,13 +12,14 @@ def greedy_decoding(model: TransformerCore,
     Performs greedy search for translating tokenized input sentence, this should be used only by autoregressive
     transformers.
     :param model: the autoregressive model.
-    :param input_ids: the tokenized input sentence of shape (batch_size, seq_len).
+    :param input_ids: the tokenized input sentence of shape (bsz, seq_len).
     :param decoder_start_token_id: the decoder start token id, for multilingual models this should be the target
         language code id.
     :param max_new_tokens: maximum allowed new tokens.
     :return: the tokenized translated sentence.
     """
     assert max_new_tokens >= 0
+
     with torch.no_grad():
         # Parameters
         max_length = input_ids.shape[-1] + max_new_tokens
@@ -33,15 +34,16 @@ def greedy_decoding(model: TransformerCore,
         eos_token_id_tensor = torch.tensor([model.eos_token_id]).unsqueeze(1).to(device)
 
         # Encode the input tokens
-        e_pad_mask = (input_ids == model.pad_token_id).to(device)
-        e_output = model.encode(input_ids, e_pad_mask=e_pad_mask)
+        e_mask = input_ids.ne(model.pad_token_id).unsqueeze(1).to(device)
+        e_output = model.encode(input_ids, e_mask)
 
         # Generate tokens in an autoregressive fashion
         for _ in range(1, max_length):
             # Obtain logits from the model's decoder
-            tgt_mask = generate_causal_mask(output.shape[-1]).to(device)
-            d_pad_mask = (output == model.pad_token_id).to(device)
-            logits = model.decode(e_output, output, tgt_mask, e_pad_mask, d_pad_mask)
+            d_pad_mask = output.ne(model.pad_token_id).unsqueeze(1).to(device)
+            d_causal_mask = generate_causal_mask(output.size(-1)).to(device)
+            d_mask = d_pad_mask & d_causal_mask
+            logits = model.decode(output, e_output, d_mask, e_mask)
 
             # Compute the new tokens and concatenate them to the previously generated ones
             logits = F.log_softmax(logits[:, -1], dim=1)
@@ -70,7 +72,7 @@ def beam_decoding(model: TransformerCore,
     Performs beams search for translating tokenized input sentence, this should be used only by autoregressive
     transformers.
     :param model: the autoregressive model.
-    :param input_ids: the tokenized input sentence of shape (batch_size, seq_len).
+    :param input_ids: the tokenized input sentence of shape (bsz, seq_len).
     :param sos_token_id: start of sentence token id.
     :param eos_token_id: end of sentence token id, for multilingual models this should be the target language code id.
     :param pad_token_id: pad token id.
@@ -80,6 +82,7 @@ def beam_decoding(model: TransformerCore,
     """
     assert max_new_tokens >= 0
     assert beam_size > 1
+
     with torch.no_grad():
         # Parameters
         max_length = input_ids.shape[-1] + max_new_tokens

src/utils/masks.py
@@ -8,7 +8,7 @@ def generate_causal_mask(seq_len: int) -> torch.Tensor:
     :param seq_len: length of the sequence to mask.
     :return: causal mask for the autoregressive decoder.
     """
-    return torch.tril(torch.ones(1, seq_len, seq_len, dtype=torch.bool), diagonal=1)
+    return torch.tril(torch.ones(1, seq_len, seq_len, dtype=torch.bool))
 
 
 def generate_causal_nat_mask(seq_len: int) -> torch.Tensor: