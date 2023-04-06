7bb62fed9566355dbf073182362e13ff5fb5a97e
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2023-04-06 11:17:08 +0200

    Implemented transformer layers

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -2,3 +2,5 @@ from .connections import ResidualConnection, HighwayConnection
 from .layers_nat import DecoderLayerNAT, DecoderNAT
 from .pooling import Fertility, Pooler
 from .positional_encoding import PositionalEncoding
+from .transformer_layers import MultiHeadAttention, FeedForwardLayer, TransformerEncoderLayer, TransformerEncoder,\
+    TransformerDecoderLayer, TransformerDecoder
