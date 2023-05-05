d44cc7b049d476ab5e737b04f4377c9423f9cc9f
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2023-05-05 23:53:50 +0200

    The datasets deal with the tokenization now

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,8 +1,12 @@
+import torch
 import datasets
 import numpy as np
+from transformers import PreTrainedTokenizerBase
+from transformers.utils import PaddingStrategy, TensorType
 from torch.utils.data import Dataset, IterableDataset
 from tqdm.auto import tqdm
 from typing import Dict, Iterator, Set, Union
+from src.utils import SUPPORTED_LANGUAGES
 
 
 class TranslationDatasetCore:
@@ -11,12 +15,27 @@ class TranslationDatasetCore:
                  src_lang: str,
                  tgt_lang: str,
                  dataset: Union[datasets.Dataset, datasets.IterableDataset],
+                 tokenizer: PreTrainedTokenizerBase,
+                 truncation: bool = True,
+                 max_length: Union[int, None] = None,
+                 padding: Union[bool, str, PaddingStrategy] = True,
+                 add_special_tokens: bool = True,
+                 return_tensors: Union[str, TensorType, None] = "pt",
+                 use_cls_token: bool = False,
                  skip_idxs: Set[int] = None) -> None:
         """
         Base class for all the translation datasets.
         :param src_lang: the source language.
         :param tgt_lang: the target language.
         :param dataset: the Huggingface dataset to wrap.
+        :param tokenizer: the tokenizer used by the collator when called.
+        :param truncation: whether to apply truncation during the tokenization (default=True).
+        :param max_length: maximum allowed length fot the tokenized sentences (default=None)
+        :param padding: the padding strategy to apply during the tokenization (defualt=True).
+        :param add_special_tokens: whether to use special tokens during the tokenization (default=True).
+        :param return_tensors: type of tensors from the tokenizer (default="pt").
+        :param use_cls_token: whether to add the cls token at the beginnning of the source sentences (default=False).
+        :param skip_idxs: the indices to skip (default=None).
         """
         # Source and target languages
         self.src_lang = src_lang
@@ -30,6 +49,18 @@ class TranslationDatasetCore:
         self.avg_length_tgt = 0
         self.max_length_src = 0
         self.max_length_tgt = 0
+
+        # Tokenizer
+        if not hasattr(tokenizer, "src_lang") or not hasattr(tokenizer, "tgt_lang"):
+            raise ValueError("You should use a tokenizer that can has \"source_lang\" and \"tgt_lang\" defined.")
+
+        if src_lang not in SUPPORTED_LANGUAGES.keys() or tgt_lang not in SUPPORTED_LANGUAGES.keys():
+            raise ValueError("There should not be an unsupported language as the source or target language.")
+
+        self.tokenizer = tokenizer
+        self.tokenizer_state = {"truncation": truncation, "max_length": max_length, "padding": padding,
+                                "add_special_tokens": add_special_tokens, "return_tensors": return_tensors}
+        self.use_cls_token = use_cls_token
 
         # Dataset's indexes that should be skipped (duplicated, corrupted or unwanted sentences)
         self.skip_idxs = set() if skip_idxs is None else skip_idxs
@@ -56,6 +87,16 @@ class TranslationDatasetCore:
         return {"max_length_src": self.max_length_src, "max_length_tgt": self.max_length_tgt,
                 "avg_length_src": self.avg_length_src, "avg_length_tgt": self.avg_length_tgt}
 
+    def tokenize_pair(self, sentence_pair: Dict[str, str]) -> Dict[str, torch.Tensor]:
+        src_sentence = sentence_pair[self.src_lang]
+        src_sentence = self.tokenizer.cls_token + " " + src_sentence if self.use_cls_token else src_sentence
+        tgt_sentence = sentence_pair[self.tgt_lang]
+        self.tokenizer.src_lang = SUPPORTED_LANGUAGES[self.src_lang]
+        self.tokenizer.tgt_lang = SUPPORTED_LANGUAGES[self.tgt_lang]
+        input_ids = self.tokenizer(src_sentence, **self.tokenizer_state)["input_ids"]
+        labels = self.tokenizer(text_target=tgt_sentence, **self.tokenizer_state)["input_ids"]
+        return {"input_ids": input_ids, "labels": labels}
+
 
 class TranslationDataset(TranslationDatasetCore, Dataset):
 
@@ -63,27 +104,41 @@ class TranslationDataset(TranslationDatasetCore, Dataset):
                  src_lang: str,
                  tgt_lang: str,
                  dataset: datasets.Dataset,
+                 tokenizer: PreTrainedTokenizerBase,
+                 truncation: bool = True,
+                 max_length: Union[int, None] = None,
+                 padding: Union[bool, str, PaddingStrategy] = True,
+                 add_special_tokens: bool = True,
+                 return_tensors: Union[str, TensorType, None] = "pt",
+                 use_cls_token: bool = False,
                  skip_idxs: Set[int] = None) -> None:
         """
         Translation dataset defined by source and target languages.
         :param src_lang: the source language.
         :param tgt_lang: the target language.
         :param dataset: the Huggingface dataset to wrap.
+        :param tokenizer: the tokenizer used by the collator when called.
+        :param truncation: whether to apply truncation during the tokenization (default=True).
+        :param max_length: maximum allowed length fot the tokenized sentences (default=None)
+        :param padding: the padding strategy to apply during the tokenization (defualt=True).
+        :param add_special_tokens: whether to use special tokens during the tokenization (default=True).
+        :param return_tensors: type of tensors from the tokenizer (default="pt").
+        :param use_cls_token: whether to add the cls token at the beginnning of the source sentences (default=False).
+        :param skip_idxs: the indices to skip (default=None).
         """
-        TranslationDatasetCore.__init__(self, src_lang, tgt_lang, dataset, skip_idxs)
+        TranslationDatasetCore.__init__(self, src_lang, tgt_lang, dataset, tokenizer, truncation, max_length, padding,
+                                        add_special_tokens, return_tensors, use_cls_token, skip_idxs)
         Dataset.__init__(self)
 
     def __len__(self) -> int:
         return len(self.dataset)
 
-    def __getitem__(self, idx) -> Dict[str, str]:
+    def __getitem__(self, idx) -> Dict[str, torch.Tensor]:
         while idx in self.skip_idxs:
             idx = np.random.randint(0, self.__len__())
 
         sentence_pair = self.dataset[idx]["translation"]
-        src_sentence = sentence_pair[self.src_lang]
-        tgt_sentence = sentence_pair[self.tgt_lang]
-        return {"src_sentence": src_sentence, "tgt_sentence": tgt_sentence}
+        return self.tokenize_pair(sentence_pair)
 
 
 class IterableTranslationDataset(TranslationDatasetCore, IterableDataset):
@@ -91,14 +146,31 @@ class IterableTranslationDataset(TranslationDatasetCore, IterableDataset):
     def __init__(self,
                  src_lang: str,
                  tgt_lang: str,
-                 dataset: datasets.IterableDataset) -> None:
+                 dataset: datasets.IterableDataset,
+                 tokenizer: PreTrainedTokenizerBase,
+                 truncation: bool = True,
+                 max_length: Union[int, None] = None,
+                 padding: Union[bool, str, PaddingStrategy] = True,
+                 add_special_tokens: bool = True,
+                 return_tensors: Union[str, TensorType, None] = "pt",
+                 use_cls_token: bool = False,
+                 skip_idxs: Set[int] = None) -> None:
         """
         Translation dataset for iterable Hugginface datasets, defined by source and target languages.
         :param src_lang: the source language.
         :param tgt_lang: the target language.
         :param dataset: the iterable Hugginface dataset to wrap.
+        :param tokenizer: the tokenizer used by the collator when called.
+        :param truncation: whether to apply truncation during the tokenization (default=True).
+        :param max_length: maximum allowed length fot the tokenized sentences (default=None)
+        :param padding: the padding strategy to apply during the tokenization (defualt=True).
+        :param add_special_tokens: whether to use special tokens during the tokenization (default=True).
+        :param return_tensors: type of tensors from the tokenizer (default="pt").
+        :param use_cls_token: whether to add the cls token at the beginnning of the source sentences (default=False).
+        :param skip_idxs: the indices to skip (default=None).
         """
-        TranslationDatasetCore.__init__(self, src_lang, tgt_lang, dataset)
+        TranslationDatasetCore.__init__(self, src_lang, tgt_lang, dataset, tokenizer, truncation, max_length, padding,
+                                        add_special_tokens, return_tensors, use_cls_token, skip_idxs)
         IterableDataset.__init__(self)
 
     def __iter__(self) -> Iterator:
@@ -109,9 +181,7 @@ class IterableTranslationDataset(TranslationDatasetCore, IterableDataset):
                     continue
 
             sentence_pair = sentence_pair_langs["translation"]
-            src_sentence = sentence_pair[self.src_lang]
-            tgt_sentence = sentence_pair[self.tgt_lang]
-            yield {"src_sentence": src_sentence, "tgt_sentence": tgt_sentence}
+            yield self.tokenize_pair(sentence_pair)
 
 
 class TextDataset(Dataset):
