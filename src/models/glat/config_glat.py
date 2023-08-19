b69713d31b35e96c1283a45718885c605abbd43a
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2023-08-19 17:15:06 +0200

    Fixed a small issue for the GLAT configuration

--- src/models/glat/config_glat.py
+++ src/models/glat/config_glat.py
@@ -21,7 +21,8 @@ class GLATConfig(NATCoreConfig):
                  pad_token_id: int = 1,
                  length_token_id: int = None,
                  label_smoothing: float = 0.0,
-                 src_embedding_copy: str = "uniform",
+                 decoder_inputs_copy: str = None,
+                 tensor_to_copy: str = "e_output",
                  pooler_size: int = 256,
                  tau: float = 0.3) -> None:
         """
@@ -45,12 +46,15 @@ class GLATConfig(NATCoreConfig):
         :param pad_token_id: the pad token id (default=1).
         :param length_token_id: the length token id, akin to a cls token (default=4).
         :param label_smoothing: the label smoothing value for the cross-entropy loss (default=0.0).
-        :param src_embedding_copy: the type of copy to apply to the source embedding, possible values: uniform, soft and
-            None (default=None).
+        :param decoder_inputs_copy: the type of copy to apply to the source embedding, possible values: uniform, soft
+            and None, with the latter no copy mechanism will be applied and the decoder input ids will be used instead
+            (default=None).
+        :param tensor_to_copy: the tensor to copy during as the decoder input, can be either "src_embeddings" or
+            "e_output". If decoder_inputs_copy is None then this parameter will have no effect (default="e_output").
         :param pooler_size: the pooler layer dimension (default=256).
         :param tau: the tau value for the soft-copy mechanism (default=0.3).
         """
         super().__init__(vocab_size, d_model, n_heads, num_encoder_layers, num_decoder_layers, dim_ff, dropout,
                          dropout_mha, dropout_ff, activation_ff, layer_norm_eps, scale_embeddings, bos_token_id,
-                         eos_token_id, pad_token_id, length_token_id, label_smoothing, src_embedding_copy, pooler_size)
-        self.tau = tau
+                         eos_token_id, pad_token_id, length_token_id, label_smoothing, decoder_inputs_copy,
+                         tensor_to_copy, pooler_size, tau)
